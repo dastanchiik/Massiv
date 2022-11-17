871eb66d36661942082305c0368a99d9f2efa4ba
dastanchick <dosyafree32@gmail.com>
2022-11-17 19:45:15 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int []win = {1,2,3,4,5,6,7,8,9,10};
+        for (int i = 0; i < win.length; i++) {
+            if (win[i]%2==0){
+                System.out.println(win[i]+" ");
+            }
+
+        }
+
+    }
+}
